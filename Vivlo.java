cb5d3c12fef7af19bfeb504502967cfb3aa8cbf6
EVogel1999 <eav990@gmail.com>
2019-05-03 14:06:36 -0400

    Configure work.java

--- Vivlo.java
+++ Vivlo.java
@@ -171,4 +171,74 @@ public class Vivlo {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
+
+    public void createSearchFrame() {
+        JFrame frame = new JFrame("Vivlo - Search");
+        JLabel JL_fname,JL_lname,JL_title,JL_isbn, JL_dept;
+        JTextField JT_fname,JT_lname,JT_title,JT_isbn;
+        JComboBox JCB_dept;
+        JButton btn_search;
+        JL_isbn = new JLabel("Enter ISBN:");
+        JL_isbn.setBounds(20, 20, 200, 20);
+        JT_isbn = new JTextField(20);
+        JT_isbn.setBounds(130, 20, 150, 20);
+        btn_search = new JButton("Search");
+        btn_search.setBounds(300, 20, 80, 20);
+        btn_search.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String isbnNum = JT_isbn.getText();
+                try {
+                    ResultSet result = query.findBook(isbnNum);
+                    if(result.next()) {
+                        JOptionPane.showMessageDialog(null, "ISBN found",
+                                "ISBN found", JOptionPane.INFORMATION_MESSAGE);
+                    }
+                    else {
+                        JOptionPane.showMessageDialog(null, "Not a valid ISBN#",
+                                "ISBN error", JOptionPane.ERROR_MESSAGE);
+                        JT_isbn.setText(null);
+                    }
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+//				String towsonID = tuID.getText();
+            }
+        });
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setSize(450,200);
+
+        JL_fname = new JLabel("Author First Name: ");
+        JL_fname.setBounds(20, 50, 100, 20);
+        JT_fname = new JTextField(20);
+        JT_fname.setBounds(130, 50, 150, 20);
+        JL_lname = new JLabel("Author Last Name: ");
+        JL_lname.setBounds(20, 80, 100, 20);
+        JT_lname = new JTextField(20);
+        JT_lname.setBounds(130, 80, 150, 20);
+        JL_title = new JLabel("Title: ");
+        JL_title.setBounds(20, 110, 100, 20);
+        JT_title = new JTextField(20);
+        JT_title.setBounds(130, 110, 150, 20);
+        JL_dept = new JLabel("Department: ");
+        JL_dept.setBounds(20,140, 100, 20);
+        String dept[] = {"Administration", "Access and Outreach Services", "Content Management", "Research Instruction"};
+        JCB_dept = new JComboBox(dept);
+        JCB_dept.setBounds(130, 140, 150, 20);
+
+        frame.setLayout(null);
+        frame.add(btn_search);
+        frame.add(JL_fname);
+        frame.add(JT_fname);
+        frame.add(JL_lname);
+        frame.add(JT_lname);
+        frame.add(JL_title);
+        frame.add(JT_title);
+        frame.add(JL_isbn);
+        frame.add(JT_isbn);
+        frame.add(JL_dept);
+        frame.add(JCB_dept);
+        frame.setVisible(true);
+    }
 }
