b1b5e77c5350e54af19b337869590902ff274f63
SHjoeOOL <44324464+SHjoeOOL@users.noreply.github.com>
2019-05-03 20:42:52 -0400

    Add files via upload

---
+++ LDdelete.sql
@@ -0,0 +1,18 @@
+use jcase4db;
+drop table REQUEST;
+drop table REQUEST_FORM;
+drop table NON_BOOK_RESOURCE;
+drop table NON_STAFF;
+drop table NREF_BOOK;
+drop table STUDENT_WORKER;
+drop table LIBRARIAN;
+drop table AS_LIBRARIAN;
+drop table LIB_STAFF;
+drop table TU_AFFILIATE;
+drop table AUTHOR;
+drop table REF_BOOK;
+drop table BOOK;
+drop table DEPART;
+drop table FLOOR;
+
+
